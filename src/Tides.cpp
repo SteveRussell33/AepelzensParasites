acb0e5782d883a3099089e7f94db8b2329ad08d6
Steve Russell <steve.russell33@gmail.com>
2023-09-11 22:53:07 +0100

    Cycles: replace 'value'

--- src/Tides.cpp
+++ src/Tides.cpp
@@ -113,26 +113,26 @@ struct Tides : Module {
 
 void Tides::process(const ProcessArgs& args) {
 	tides::GeneratorMode mode = generator.mode();
-	if (modeTrigger.process(params[MODE_PARAM].value)) {
+	if (modeTrigger.process(params[MODE_PARAM].getValue())) {
 		mode = (tides::GeneratorMode) (((int)mode - 1 + 3) % 3);
 		generator.set_mode(mode);
 	}
-	lights[MODE_GREEN_LIGHT].value = (mode == 2) ? 1.0 : 0.0;
-	lights[MODE_RED_LIGHT].value = (mode == 0) ? 1.0 : 0.0;
+	lights[MODE_GREEN_LIGHT].setBrightness((mode == 2) ? 1.0 : 0.0);
+	lights[MODE_RED_LIGHT].setBrightness((mode == 0) ? 1.0 : 0.0);
 
 	tides::GeneratorRange range = generator.range();
-	if (rangeTrigger.process(params[RANGE_PARAM].value)) {
+	if (rangeTrigger.process(params[RANGE_PARAM].getValue())) {
 		range = (tides::GeneratorRange) (((int)range - 1 + 3) % 3);
 		generator.set_range(range);
 	}
-	lights[RANGE_GREEN_LIGHT].value = (range == 2) ? 1.0 : 0.0;
-	lights[RANGE_RED_LIGHT].value = (range == 0) ? 1.0 : 0.0;
+	lights[RANGE_GREEN_LIGHT].setBrightness((range == 2) ? 1.0 : 0.0);
+	lights[RANGE_RED_LIGHT].setBrightness((range == 0) ? 1.0 : 0.0);
 
 	//Buffer loop
 	if (generator.writable_block()) {
 		// Pitch
-		float pitchParam = clamp(params[FREQUENCY_PARAM].value + inputs[PITCH_INPUT].value * 12.0f, -60.0f, 60.0f);
-		float fm = clamp(inputs[FM_INPUT].value /5.0f * params[FM_PARAM].value /12.0f, -1.0f, 1.0f) * 0x600;
+		float pitchParam = clamp(params[FREQUENCY_PARAM].getValue() + inputs[PITCH_INPUT].getVoltage() * 12.0f, -60.0f, 60.0f);
+		float fm = clamp(inputs[FM_INPUT].getVoltage() / 5.0f * params[FM_PARAM].getValue() / 12.0f, -1.0f, 1.0f) * 0x600;
 
 		pitchParam += 60.0;
 		//this is probably not original but seems useful to keep the same frequency as in normal mode
@@ -156,18 +156,18 @@ void Tides::process(const ProcessArgs& args) {
 		    generator.set_pitch_high_range(clamp(pitch, -0x8000, 0x7fff), fm);
 		}
 		else {
-		    generator.set_pitch(clamp(pitch, -0x8000, 0x7fff),fm);
+		    generator.set_pitch(clamp(pitch, -0x8000, 0x7fff), fm);
 		}
 
 		if (generator.feature_mode_ == tides::Generator::FEAT_MODE_RANDOM) {
 		    //TODO: should this be inverted?
-		    generator.set_pulse_width(clamp(1.0 - params[FM_PARAM].value /12.0, 0.0f, 2.0f) * 0x7fff);
+		    generator.set_pulse_width(clamp(1.0 - params[FM_PARAM].getValue() / 12.0, 0.0f, 2.0f) * 0x7fff);
 		}
 
 		// Slope, smoothness, pitch
-		int16_t shape = clamp(params[SHAPE_PARAM].value + inputs[SHAPE_INPUT].value / 5.0f, -1.0f, 1.0f) * 0x7fff;
-		int16_t slope = clamp(params[SLOPE_PARAM].value + inputs[SLOPE_INPUT].value / 5.0f, -1.0f, 1.0f) * 0x7fff;
-		int16_t smoothness = clamp(params[SMOOTHNESS_PARAM].value + inputs[SMOOTHNESS_INPUT].value / 5.0f, -1.0f, 1.0f) * 0x7fff;
+		int16_t shape = clamp(params[SHAPE_PARAM].getValue() + inputs[SHAPE_INPUT].getVoltage() / 5.0f, -1.0f, 1.0f) * 0x7fff;
+		int16_t slope = clamp(params[SLOPE_PARAM].getValue() + inputs[SLOPE_INPUT].getVoltage() / 5.0f, -1.0f, 1.0f) * 0x7fff;
+		int16_t smoothness = clamp(params[SMOOTHNESS_PARAM].getValue() + inputs[SMOOTHNESS_INPUT].getVoltage() / 5.0f, -1.0f, 1.0f) * 0x7fff;
 		generator.set_shape(shape);
 		generator.set_slope(slope);
 		generator.set_smoothness(smoothness);
@@ -188,11 +188,11 @@ void Tides::process(const ProcessArgs& args) {
 		level = 0;
 
 	uint8_t gate = 0;
-	if (inputs[FREEZE_INPUT].value >= 0.7)
+	if (inputs[FREEZE_INPUT].getVoltage() >= 0.7)
 		gate |= tides::CONTROL_FREEZE;
-	if (inputs[TRIG_INPUT].value >= 0.7)
+	if (inputs[TRIG_INPUT].getVoltage() >= 0.7)
 		gate |= tides::CONTROL_GATE;
-	if (inputs[CLOCK_INPUT].value >= 0.7)
+	if (inputs[CLOCK_INPUT].getVoltage() >= 0.7)
 		gate |= tides::CONTROL_CLOCK;
 	if (!(lastGate & tides::CONTROL_CLOCK) && (gate & tides::CONTROL_CLOCK))
 		gate |= tides::CONTROL_GATE_RISING;
@@ -212,10 +212,10 @@ void Tides::process(const ProcessArgs& args) {
 	float unif = (float) uni / 0xffff;
 	float bif = (float) bi / 0x8000;
 
-	outputs[HIGH_OUTPUT].value = sample.flags & tides::FLAG_END_OF_ATTACK ? 0.0 : 5.0;
-	outputs[LOW_OUTPUT].value = sample.flags & tides::FLAG_END_OF_RELEASE ? 0.0 : 5.0;
-	outputs[UNI_OUTPUT].value = unif * 8.0;
-	outputs[BI_OUTPUT].value = bif * 5.0;
+	outputs[HIGH_OUTPUT].setVoltage(sample.flags & tides::FLAG_END_OF_ATTACK ? 0.0 : 5.0);
+	outputs[LOW_OUTPUT].setVoltage(sample.flags & tides::FLAG_END_OF_RELEASE ? 0.0 : 5.0);
+	outputs[UNI_OUTPUT].setVoltage(unif * 8.0);
+	outputs[BI_OUTPUT].setVoltage(bif * 5.0);
 
 	if (sample.flags & tides::FLAG_END_OF_ATTACK)
 		unif *= -1.0;
